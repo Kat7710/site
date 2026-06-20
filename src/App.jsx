2a88fd90561d05900b11fe10963b51b589a4eb97
Kat7710 <erik.odessa2009@gmail.com>
2026-06-20 13:37:02 +0300

    added second page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Career from "./components/Career";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+
+        <Route path="/career" element={<Career />} />
+      </Routes>
+
       <Footer />
     </div>
   );

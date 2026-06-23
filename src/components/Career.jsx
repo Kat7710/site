68ad995b9c0bc3d695be4339a32eb6a9351ce2cb
WhItty-39 <whtty1001011@gmail.com>
2026-06-23 16:28:01 +0300

    Finaly some normal changes

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import "./Career.scss";
 
 function CAreer() {
   return <>
-    <div className="career-form">
-      <h2>Want to join us?</h2>
-      <p>Then just fill this form!</p>
-      
-    </div>
-    
-    
+    <div className="career-window">
+      <div className="career-title">
+        <h2 className="career-main-title">Want to join us?</h2>
+        <p>Then just fill this form!</p>
+      </div>     
+      <form className="career-form" onSubmit={(e) => e.preventDefault()}>
+        <input type="text" className="career-input" placeholder="Enter your name" required />
+        <input type="text" className="career-input" placeholder="Enter your surname" required />
+        <input type="email" className="career-input" placeholder="Enter your email" required />
+        <button type="submit" className="career-submit-btn">
+          Send
+        </button>
+      </form>
+    </div>  
   </>;
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,22 +5,6 @@ import "./Footer.scss";
 function Footer() {
   return (
     <footer className="footer">
-      {/* <div className="footer-contact">
-        <p className="footer-title">AUTONOM</p>
-        <div className="contacts">
-          <ul className="contact-items">
-            <li>
-              <Link to="/">Technology</Link>
-            </li>
-            <li>
-              <Link to="/">About us</Link>
-            </li>
-            <li>
-              <Link to="/career">Career</Link>
-            </li>
-          </ul>
-        </div>
-      </div> */}
       <p className="copyright">© 2026 AUTONOM</p>
     </footer>
   );
